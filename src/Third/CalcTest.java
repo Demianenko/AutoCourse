07d9719bdc49c8a877b6b41dbf23bb2215db69e4
Demianenko <erlond@narod.ru>
2016-05-31 18:51:48 +0300

    5

--- src/Third/CalcTest.java
+++ src/Third/CalcTest.java
@@ -13,6 +13,30 @@ public class CalcTest {
         Assert.assertEquals(calculator.calc(0,0,"+"),0.0);
         Assert.assertEquals(calculator.calc(-1,-1,"+"),-2.0);
         Assert.assertEquals(calculator.calc(-1,1,"+"),0.0);
+        Assert.assertEquals(calculator.calc(Double.MAX_VALUE,Double.MAX_VALUE,"+"),Double.POSITIVE_INFINITY);
+    }
+    @Test(description = "Test -", enabled = true)
+    public void testMinus(){
+        Assert.assertEquals(calculator.calc(0,0,"-"),0.0);
+        Assert.assertEquals(calculator.calc(-1,-1,"-"),0.0);
+        Assert.assertEquals(calculator.calc(-1,1,"-"),-2.0);
+        Assert.assertEquals(calculator.calc(Double.MIN_VALUE,-Double.MIN_VALUE,"-"),Double.NEGATIVE_INFINITY);
+    }
+    @Test(description = "Test *", enabled = true)
+    public void testMulti(){
+        Assert.assertEquals(calculator.calc(0,0,"*"),0.0);
+        Assert.assertEquals(calculator.calc(-1,-1,"*"),1.0);
+        Assert.assertEquals(calculator.calc(-1,1,"*"),-1.0);
+        Assert.assertEquals(calculator.calc(Double.MIN_VALUE,0,"*"),0.0);
+        Assert.assertEquals(calculator.calc(Double.MAX_VALUE,Double.MAX_VALUE,"*"),Double.POSITIVE_INFINITY);
+    }
+    @Test(description = "Test /", enabled = true)
+    public void testDev(){
+        Assert.assertEquals(calculator.calc(0,0,"/"),Double.NaN);
+        Assert.assertEquals(calculator.calc(-1,-1,"/"),1.0);
+        Assert.assertEquals(calculator.calc(-1,1,"/"),-1.0);
+        Assert.assertEquals(calculator.calc(Double.MIN_VALUE,0,"/"),Double.POSITIVE_INFINITY);
+        Assert.assertEquals(calculator.calc(Double.MAX_VALUE,Double.MAX_VALUE,"/"),1.0);
 
     }
 }

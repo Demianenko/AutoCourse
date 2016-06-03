51d141cb902020427cafccf83ef9897a52382bc7
Demianenko <erlond@narod.ru>
2016-06-03 10:23:47 +0300

    four h/w

--- src/Four/HomeWork/CalcTest.java
+++ src/Four/HomeWork/CalcTest.java
@@ -8,19 +8,19 @@ public class CalcTest {
 
     Calculator calculator = new Calculator();
 
-    @Test(dataProvider = "getDataPlus",dataProviderClass = CalcTestData.class, description = "Test +", enabled = true)
+    @Test(dataProvider = "getDataPlus",dataProviderClass = CalcTestData.class, description = "Test +", enabled = true, groups = {"smoke","p1"})
     public void testPlus(double a, double b, double c){
         Assert.assertEquals(calculator.calc(a,b,"+"),c);
     }
-    @Test(dataProvider = "getDataMinus", dataProviderClass = CalcTestData.class,description = "Test -", enabled = true)
+    @Test(dataProvider = "getDataMinus", dataProviderClass = CalcTestData.class,description = "Test -", enabled = true, groups = "p1")
     public void testMinus(double a, double b, double c) {
         Assert.assertEquals(calculator.calc(a, b, "-"), c);
     }
-    @Test(dataProvider = "getDataMulti",dataProviderClass = CalcTestData.class, description = "Test *", enabled = true)
+    @Test(dataProvider = "getDataMulti",dataProviderClass = CalcTestData.class, description = "Test *", enabled = true, groups = "p2")
     public void testMulti(double a, double b, double c){
         Assert.assertEquals(calculator.calc(a,b,"*"),c);
     }
-    @Test(dataProvider = "getDataDev", dataProviderClass = CalcTestData.class, description = "Test /", enabled = true)
+    @Test(dataProvider = "getDataDev", dataProviderClass = CalcTestData.class, description = "Test /", enabled = true, groups = "p3")
     public void testDev(double a, double b, double c){
         Assert.assertEquals(calculator.calc(a,b,"/"),c);
     }
--- src/Four/OrderTEst.java
+++ src/Four/OrderTEst.java
@@ -8,27 +8,27 @@ import org.testng.annotations.Test;
  */
 public class OrderTEst {
 
-    @Test(description = "Test to test 1", enabled = true)
+    @Test(description = "Test to test 1", enabled = true, groups = {"p2","smoke"})
     public void test1() {
         System.out.println("Test # 1");
     }
 
-    @Test(description = "Test to test 2" , enabled = true)
+    @Test(description = "Test to test 2" , enabled = true, groups = {"p3","smoke"})
     public void test2()  {
         System.out.println("Test # 2");
     }
 
-    @Test(description = "Test to test 3", enabled = true)
+    @Test(description = "Test to test 3", enabled = true, groups = {"p3"})
     public void test3() {
         System.out.println("Test # 3");
     }
 
-    @Test(description = "Test to test 4", enabled = true)
+    @Test(description = "Test to test 4", enabled = true, groups = {"p3"})
     public void test4() {
         System.out.println("Test # 4");
     }
 
-    @Test(description = "Test to test 5", enabled = true)
+    @Test(description = "Test to test 5", enabled = true, groups = {"p3"})
     public void test5(){
         System.out.println("Test # 5");
     }
